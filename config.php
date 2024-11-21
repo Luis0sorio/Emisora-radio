e6d76d6067704cdb679de29b9ecf92bf1a5699b3
Luis0sorio <luiseosorio96@gmail.com>
2024-11-21 11:24:47 +0100

    emisora de radio - archivos config y conexion de la base de datos

---
+++ config.php
@@ -0,0 +1,6 @@
+
+<?php 
+
+define ("DSN", "mysql:host=localhost; port=3307;dbname=emidoradb2");
+define ("USER", "root");
+define ("PASS", "");
---
+++ database/conexion.php
@@ -0,0 +1,14 @@
+
+<?php 
+
+require_once "./config.php";
+
+function conexionDB() {
+  try {
+    $conexion = new PDO (DSN, USER, PASS);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $conexion;
+  } catch (PDOException $e) {
+    echo "Error al establecer la conexion con la base de datos. " . $e->getMessage();
+  }
+}
